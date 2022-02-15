fae77bd68e411254067023b1cb9ac3b803076e2c
Krunal Parmar <kgparmar6@gmail.com>
2022-02-15 20:17:16 +0530

    database connectivity

---
+++ src/main/java/com/controller/RoleController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.RoleBean;
+
+@Controller
+public class RoleController {
+	
+	@GetMapping("/newrole")
+	public String newRole() {
+		return "NewRole";
+	}
+	
+	@PostMapping("/saverole")
+	public String saveRole(RoleBean role) {
+	
+		System.out.println(role.getRoleName());
+		return "";
+	}
+
+}
